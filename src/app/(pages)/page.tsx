db2f04b5e798ea4d4099fe5b2bcd1b89b5aabe6a
sieun.lee <se.lee@purple.io>
2024-12-27 00:56:22 +0900

    인트로 모션 작업 완료

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,16 +1,31 @@
+"use client";
+
+import Box from "@/components/Box";
+import { motion } from "framer-motion";
+
 const Home = () => {
   return (
-    <div className="h-full w-full overflow-hidden overflow-y-auto p-6">
-      <div className="mb-2">
-        <h1 className=" text-3xl font-semibold">Welcome back</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4"></div>
-      </div>
-      <div className="mt-2 mb-7 px-6">
-        <div className="flex justify-between items-center">
-          <h1 className=" text-2xl font-semibold">Newest songs</h1>
-        </div>
-        <div>List of Songs</div>
-      </div>
+    <div className="relative h-[100vh] min-w-[1000px] min-h-[650px] p-[5vh] overflow-x-hidden">
+      <ul className="flex w-full h-full">
+        <li className="left w-1/2 h-full bg-[#58CE8F]"></li>
+        <li className="right w-1/2 h-full bg-[#FBFBFB]"></li>
+      </ul>
+      <Box className="text-[11.5rem] absolute top-1/2 left-1/3 transform -translate-x-1/3 -translate-y-1/2 leading-none select-none flex items-end w-auto">
+        <motion.h1
+          initial={{ x: -400, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+        >
+          <div className="font-extrabold">SIEUN</div>
+          <div className="font-semibold">PORTFOLIO</div>
+        </motion.h1>
+        <motion.div
+          initial={{ x: "100vw" }}
+          animate={{ x: 0 }}
+          transition={{ duration: 2, delay: 0, ease: "easeInOut" }}
+          className="bg-[#58CE8F] w-[50px] h-[50px] rounded-full"
+        />
+      </Box>
     </div>
   );
 };

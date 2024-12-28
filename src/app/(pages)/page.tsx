1c08865ca6f2ec729167a0b0c535cf10590ea9eb
sieun.lee <se.lee@purple.io>
2024-12-28 20:10:38 +0900

    메인 반응형 추가

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -5,25 +5,29 @@ import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <div className="relative h-[100vh] min-w-[1000px] min-h-[650px] p-[5vh] overflow-x-hidden">
-      <ul className="flex w-full h-full">
-        <li className="left w-1/2 h-full bg-[#58CE8F]"></li>
-        <li className="right w-1/2 h-full bg-[#FBFBFB]"></li>
-      </ul>
-      <Box className="text-[11.5rem] absolute top-1/2 left-1/3 transform -translate-x-1/3 -translate-y-1/2 leading-none select-none flex items-end w-auto">
+    <div className="relative h-[100vh] min-w-[375px] overflow-x-hidden p-[15px] md:p-[30px] lg:p-[5vh]">
+      <Box className="block md:flex w-full h-full">
+        <div className="w-full h-1/2 md:left md:w-1/2 md:h-full bg-[#58CE8F]"></div>
+        <div className="w-full h-1/2 md:right md:w-1/2 md:h-full bg-[#FBFBFB]"></div>
+      </Box>
+      <Box className="text-[11.5rem] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 leading-none select-none flex items-end w-auto gap-2">
         <motion.h1
           initial={{ x: -400, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 1, ease: "easeInOut" }}
         >
-          <div className="font-extrabold">SIEUN</div>
-          <div className="font-semibold">PORTFOLIO</div>
+          <div className="font-extrabold text-[40px] md:text-[80px] lg:text-[180px]">
+            SIEUN
+          </div>
+          <div className="font-semibold text-[40px] md:text-[80px] lg:text-[160px]">
+            PORTFOLIO
+          </div>
         </motion.h1>
         <motion.div
           initial={{ x: "100vw" }}
           animate={{ x: 0 }}
           transition={{ duration: 2, delay: 0, ease: "easeInOut" }}
-          className="bg-[#58CE8F] w-[50px] h-[50px] rounded-full"
+          className="bg-[#58CE8F] rounded-full w-[20px] h-[20px] mb-[5px] md:w-[30px] md:h-[30px] md:mb-[10px] lg:w-[50px] lg:h-[50px] lg:mb-[20px]"
         />
       </Box>
     </div>

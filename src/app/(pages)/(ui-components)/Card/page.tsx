5aa8e424b17ad3c1748ef63c723304273e691512
sieun.lee <se.lee@purple.io>
2025-01-04 12:54:59 +0900

    card 페이지 추가

---
+++ src/app/(pages)/(ui-components)/Card/page.tsx
@@ -0,0 +1,9 @@
+const CardPage = () => {
+  return (
+    <article className="flex flex-col gap-5">
+      <div className="text-xl font-bold">Card</div>
+    </article>
+  );
+};
+
+export default CardPage;

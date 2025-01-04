aacd8ebf85ba1ee68d69b3871876c12f43e6226b
sieun.lee <se.lee@purple.io>
2025-01-04 13:22:31 +0900

    라우터 이동 타입 추가

--- src/components/Side-bar.tsx
+++ src/components/Side-bar.tsx
@@ -10,19 +10,30 @@ interface SideBarProps {
 }
 
 const menuList = [
-  { category: "About", list: [{ title: "Experience", link: "/Experience" }] },
+  {
+    category: "About",
+    list: [
+      { title: "Experience", link: "/Experience", type: "router" },
+      { title: "GitHub", link: "https://github.com/Sienna98", type: "window" },
+      {
+        title: "Blog",
+        link: "https://velog.io/@tldms0827/posts",
+        type: "window",
+      },
+    ],
+  },
   {
     category: "UI-component",
     list: [
-      { title: "Alert", link: "/Alert" },
-      { title: "Button", link: "/Button" },
-      { title: "Card", link: "/Card" },
-      { title: "Checkbox", link: "/Checkbox" },
-      { title: "Confirm", link: "/Confirm" },
-      { title: "Input", link: "/Input" },
-      { title: "Modal", link: "/Modal" },
-      { title: "Progress", link: "/Progress" },
-      { title: "Selector", link: "/Selector" },
+      { title: "Alert", link: "/Alert", type: "router" },
+      { title: "Button", link: "/Button", type: "router" },
+      { title: "Card", link: "/Card", type: "router" },
+      { title: "Checkbox", link: "/Checkbox", type: "router" },
+      { title: "Confirm", link: "/Confirm", type: "router" },
+      { title: "Input", link: "/Input", type: "router" },
+      { title: "Modal", link: "/Modal", type: "router" },
+      { title: "Progress", link: "/Progress", type: "router" },
+      { title: "Selector", link: "/Selector", type: "router" },
     ],
   },
   {
@@ -31,8 +42,13 @@ const menuList = [
       {
         title: "Purple Place",
         link: "https://purple-place.vercel.app",
+        type: "window",
       },
-      { title: "Jamongda", link: "https://jamongda.netlify.app" },
+      {
+        title: "Jamongda",
+        link: "https://jamongda.netlify.app",
+        type: "window",
+      },
     ],
   },
   {
@@ -41,11 +57,13 @@ const menuList = [
       {
         title: "Starbucks",
         link: "https://elegant-begonia-dba8ef.netlify.app",
+        type: "window",
       },
       { title: "Movv", link: "https://unrivaled-longma-60ac49.netlify.app" },
       {
         title: "Nongshim nav",
         link: "https://silver-frangollo-742fe8.netlify.app",
+        type: "window",
       },
     ],
   },
@@ -79,10 +97,7 @@ const SideBar: React.FC<SideBarProps> = ({ children }) => {
                         <h3
                           className="relative z-10 transition-all duration-300 transform"
                           onClick={() => {
-                            if (
-                              menu.category === "UI-component" ||
-                              menu.category === "About"
-                            ) {
+                            if (item.type === "router") {
                               router.push(item.link);
                             } else {
                               window.open(item.link, "_blank");

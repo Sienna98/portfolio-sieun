fd0ceb5959b7650b287ecf189dc92a2bbfb8bd15
sieun.lee <se.lee@purple.io>
2025-01-11 20:20:08 +0900

    ui component 모바일 레이아웃 추가

--- src/app/(pages)/(ui-components)/layout.tsx
+++ src/app/(pages)/(ui-components)/layout.tsx
@@ -1,7 +1,7 @@
 const UiComponentsLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-[100vh] flex items-center justify-center">
-      <div className="py-[100px] px-[150px] shadow-md shadow-[rgba(0,0,0,0.05)]">
+      <div className="md:py-[100px] md:px-[150px] md:shadow-md md:shadow-[rgba(0,0,0,0.05)]">
         {children}
       </div>
     </div>

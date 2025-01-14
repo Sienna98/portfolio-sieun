1399b06f7788764f519acb273e3bc06bab24e2f9
sieun.lee <se.lee@purple.io>
2025-01-14 21:13:26 +0900

    meta 태그 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,11 @@
-import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 
-import "../styles/globals.css";
-import FloatingButton from "@/components/floating-button";
 import DesktopSideBar from "@/components/desktop-side-bar";
+import FloatingButton from "@/components/floating-button";
 import { ToastContainer } from "react-toastify";
+import "../styles/globals.css";
 
 const figTree = Figtree({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "SIEUN PORTFOLIO",
-  description: "프론트엔드 개발자 이시은 포트폴리오",
-};
 
 export default function RootLayout({
   children,
@@ -20,6 +14,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:title" content="SIEUN PORTFOLIO" />
+        <meta
+          property="og:description"
+          content="프론트엔드 개발자 이시은 포트폴리오"
+        />
+        <meta property="og:image" content="/images/thumbnail.png" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://sieun-portfolio.netlify.app/"
+        />
+      </head>
       <body className={`${figTree.className}`}>
         <DesktopSideBar>{children}</DesktopSideBar>
         <FloatingButton />
